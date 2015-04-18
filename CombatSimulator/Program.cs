4e1500736f95ac1f5d0d9c4b65efdd14fc33f681
ColtonMiller <ColtonJMMiller@gmail.com>
2015-04-18 12:56:57 -0600

    Added Gui and have user attack and heal working

--- CombatSimulator/Program.cs
+++ CombatSimulator/Program.cs
@@ -9,41 +9,160 @@ namespace CombatSimulator
     class Program
     {
         //bool values for playing and play again
-        bool playing = true;
-        bool playAgain = true;
+        static bool playing = true;
+        static bool playAgain = true;
         //counter for health of user and enemy
-        int UserHealth = 100;
-        int EnemyHealth = 200;
+        static int UserHealth = 100;
+        static int EnemyHealth = 200;
         //random generators 
-        Random rng = new Random();
-        Random rng2 = new Random();
+        static Random rng = new Random();
+        static Random rng2 = new Random();
         //users inital input 
-        string usersInput = string.Empty;
+        static string usersInput = string.Empty;
         static int intUserInput = 0;
         //valid user responce
         static int validUserInput = 0;
         static void Main(string[] args)
         {
+           //start game with startscreen
+            StartingScreen();
+           //loops through game until playing equals false
+            while (playing == true)
+            {
+                DisplayStats();
+                //makes users input set to console read line
+                usersInput = Console.ReadLine();
+                //validate userinput
+                ValidateUserInput(usersInput);
+                //if input is 1 or 2 use Attack function else use heal function
+                if (validUserInput == 1 || validUserInput == 2)
+                {
+                    Attack(validUserInput);
+                }
+                else if (validUserInput == 3)
+                {
+                    Heal();
+                }
+                //clear screen  to make it look static
+                Console.Clear();
+
+            }
         }
+        /// <summary>
+        /// checks that user input is either 1 2 or 3 
+        /// </summary>
+        /// <param name="userInput">takes in the string of userInput</param>
         public static void ValidateUserInput (string userInput) 
         {
-           int.TryParse(userInput, out intUserInput);
-            switch (intUserInput)
+            //make a switch with 3 cases of input being either 1 2 or 3 else it is an invalid entry
+            switch (userInput)
 	        {
-		        case 1:
+		        case "1":
                     validUserInput = 1;
                  break;
-                case 2:
+                case "2":
                     validUserInput = 2;
                  break;
-                case 3: 
+                case "3": 
                     validUserInput = 3;
                  break;
                 
                 default:
                     Console.WriteLine("Invalid Entry");
+                    System.Threading.Thread.Sleep(1000);
                  break;
 	        }
+        }
+        /// <summary>
+        /// Function for the begining of the game
+        /// </summary>
+        public static void StartingScreen()
+        {
+            Console.WriteLine("                       Welcome to Combat Simulator Warrior                                ");
+            Console.WriteLine("                            press any key to continue                                     ");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("   In this game you will fight for your life against a mighty Ice dragon that");
+            Console.WriteLine("");
+            Console.WriteLine("          can breath fire...I know it makes no sense but just...okay?");
+            Console.WriteLine("");
+            Console.WriteLine("  I mean really though ice dragon breathing fire thats like terrible evolution");
+            Console.WriteLine("");
+            Console.WriteLine("      Any way try to just look past that and hit any key to continue....");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        /// <summary>
+        /// gives ability info health and tells user to pick 1 2 or 3
+        /// </summary>
+        public static void DisplayStats()
+        {
+            Console.WriteLine("1: Is your Sword attack it can hit from 20-35 HP but only has a 70% chance to hit" );
+            Console.WriteLine("2: Is your fire ball which actually does damage to the fire breathing ice dragon somehow...anyway it always hit for 10-15 HP");
+            Console.WriteLine("3: Is your heal that regenerates 10-20 HP");
+            Console.WriteLine("");
+            Console.WriteLine("Remember the dragon attacks after you do with an 80% chance to hit for 5-15 HP");
+            Console.WriteLine("");
+            Console.WriteLine("Your Health: " + UserHealth);
+            Console.WriteLine("Dragon's Health: " + EnemyHealth);
+            Console.WriteLine("");
+            Console.WriteLine("Select your ability by entering 1, 2, or 3");
+        }
+        /// <summary>
+        /// does all the random instances of attacking and aplies to 
+        /// </summary>
+        /// <param name="number">users validated input</param>
+        public static void Attack(int number)
+        {
+            //checks if attack is sword or fireball
+            if (number == 1)
+            {
+                //attack is sword
+                //make random chance to see if hits
+                int swordChance = rng.Next(1, 11);
+                //if it is 1-7 it hits else it doesn't
+                if (swordChance < 8) 
+                {
+                    //hit
+                    //set sword to random number between 20 to 35 take away that much from dragons health
+                    int sword = rng.Next(20, 36);
+                    EnemyHealth -= sword;
+                    Console.WriteLine("Your sword hit and hit the dragon for " + sword);
+                    System.Threading.Thread.Sleep(4000);
+                }
+                else if (swordChance >= 8)
+                {
+                    //didn't hit
+                    Console.WriteLine("Your swing of the sword missed and didn't hurt the dragon");
+                    System.Threading.Thread.Sleep(4000);
+                }
+                //dragons turn to attack
+            }
+            else
+            {
+                //attack it fireball
+                //set fireball to random number between 10-15 take away that much from dragons health
+                int fireball = rng.Next(10, 16);
+                EnemyHealth -= fireball;
+                Console.WriteLine("You hit the dragon for " + fireball);
+                System.Threading.Thread.Sleep(4000);
+            }
+        }
+        /// <summary>
+        /// heals user with a max of 100 
+        /// </summary>
+        public static void Heal()
+        {
+            //set random number for heal add to user's HP
+            int heal = rng.Next(10, 21);
+            UserHealth += heal;
+            //users health is over 100 set to 100
+            if (UserHealth > 100)
+            {
+                UserHealth = 100;
+            }
+            Console.WriteLine("You heal for " + heal);
+            System.Threading.Thread.Sleep(5000);
         }
     }
 }
